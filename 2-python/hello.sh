c20d5af700fa472975d4113d878f9af90e41cc07
ChildhoodMoments <lizhao.du@outlook.com>
2022-01-26 18:35:36 -0600

    some python stuff about math

---
+++ 2-python/hello.sh
@@ -0,0 +1,4 @@
+
+echo "My computer is a windows HP PC" > office/computer.txt
+touch office/camera.txt
+
---
+++ 2-python/numbers.py
@@ -0,0 +1,22 @@
+my_int = 1234
+print(type(my_int))
+
+my_float_2 = 4.0
+print(my_float_2)
+print(type(my_float_2))
+
+
+my_complex = 3.14+ 2.123
+print(my_complex)
+print(type(my_complex))
+
+print(type(12/3))
+
+# force get integer
+print(type(12//3))
+print(type(11//3))
+
+
+exit()
+
+
